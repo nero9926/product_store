774e289e6c67d12b781f9f0ee467402e591f1183
Nikita <sozontov.nikita@yandex.ru>
2024-09-30 18:04:37 +0300

    upd

--- app/order_worker/main.py
+++ app/order_worker/main.py
@@ -1,9 +1,27 @@
 import json
 import os
 import sys
+import uuid as uuid_pkg
+from datetime import date, datetime, timedelta
+from typing import Any, Dict
 
 import pika
 from dotenv import load_dotenv
+from sqlalchemy import (UUID, Column, Date, DateTime, Double, ForeignKey,
+                        MetaData, String, create_engine)
+from sqlalchemy.ext.declarative import as_declarative
+from sqlalchemy.orm import Session, relationship
+
+postgres_metadata = MetaData()
+
+
+@as_declarative(metadata=postgres_metadata)
+class Base:
+    id: Any
+
+    def __init__(self, **kwargs: Dict) -> None:
+        pass
+
 
 # import app.services.order as service
 # from ..api.deps import get_db_pg
@@ -12,13 +30,40 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+class Order(Base):
+    __tablename__ = "order"
+
+    id = Column(UUID, default=uuid_pkg.uuid4, primary_key=True)
+    customer_id = Column(UUID, ForeignKey('user.id'))
+    total = Column(Double, nullable=False)
+    date_placed = Column(DateTime(), default=datetime.now)
+    deliver_date = Column(Date(), default=date.today() +
+                          timedelta(days=7), nullable=False)
+    status = Column(String(30), default='created')
+    products = relationship("Order_Product", backref="order")
+    payment_details_id = relationship(
+        "PaymentDetails", backref="order", uselist=False)
+
+
 def main():
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")[0],
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")[0],
+    POSTGRES_HOST = os.environ.get("POSTGRES_HOST")[0],
+    POSTGRES_PORT = int(os.environ["POSTGRES_PORT"]),
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")[0],
+
+    print(POSTGRES_PORT[0])
+    POSTGRES_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"  # noqa
+
+    engine = create_engine(POSTGRES_URL)
+    session = Session(bind=engine)
+
     # rabbitmq connection
     credentials = pika.PlainCredentials(
         os.environ.get("RABBITMQ_USER"), os.environ.get("RABBITMQ_PASSWORD"))
 
     connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=os.environ.get("RABBITMQ_HOST"), port=os.environ.get("RABBITMQ_PORT"),
+        host=os.environ.get("RABBITMQ_HOST"), port=os.environ.get("RABBITMQ_PORT"),  # noqa
         credentials=credentials))
 
     channel = connection.channel()
@@ -27,10 +72,11 @@ def main():
         try:
             order = json.loads(body)
             print(f"Received order: {order['order_uuid']}")
-            # Проверка наличия
-            # payload = {
-            #     'status': 'in_work'
-            # }
+            db_instance = session.query(Order).get(order['order_uuid'])
+            db_instance.status = 'in_work'
+            session.add(db_instance)
+            session.commit()
+
             print('cheking....')
             order['order_status'] = 'in_work'
             # changed_order = patch_order(order_uuid=order.order_uuid,
